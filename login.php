19957874cfefc6891ce3ae8ce4ff45de74992464
Marina TAKEMOTO <>
2021-01-18 21:09:49 +0900

    ログインを短時間に複数回試行するとロックがかかるように変更（ブルートフォース対策） #5

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 session_start();
 require(dirname(__FILE__) . '/common/php_header.php');
 $error = NULL;
+date_default_timezone_set('Asia/Tokyo');
 // POSTされた時にエラーチェックをする
 if (!empty($_POST)) {
     if ($_POST['email'] !== '' && $_POST['password'] !== '') {
@@ -12,15 +13,49 @@ if (!empty($_POST)) {
         $login->execute(array(
             $_POST['email'],
         ));
-        $member = $login->fetch();
+        $member = $login->fetch(PDO::FETCH_ASSOC);
+        $latest_login = strtotime($member['latest_login']);
+        $now = strtotime(date('Y-m-d H:i:s'));
+        $try_login = $member['try_login'];
     }
-    //登録したパスワードと一致していればログイン完了
-    if (password_verify($_POST['password'], $member['password'])) {
-        $_SESSION['login'] = $_POST;
-        header('Location: new_post.php');
-        exit();
+    //10分以内にログイン試行していなければ$try_loginを0にする
+    if (($now - $latest_login) >= 600) {
+        $try_login = 0;
+    }
+    if ($try_login !== 'locked') {
+        //登録したパスワードと一致していればログイン完了
+        if (password_verify($_POST['password'], $member['password'])) {
+            $_SESSION['login']['email'] = $member['email'];
+            $_SESSION['login']['member_id'] = $member['member_id'];
+            //最終ログインとログイン試行回数をアップデート
+            $update_login = $db->prepare(
+                'UPDATE members SET latest_login=NOW(), try_login="0" WHERE member_id=?'
+            );
+            $update_login->execute(array($member['member_id']));
+            header('Location: new_post.php');
+            exit();
+        } else if ($try_login <= 2) {
+            $error = 'wrong';
+            //最終ログインとログイン試行回数をアップデート
+            $try_login += 1;
+            $update_login = $db->prepare(
+                'UPDATE members SET latest_login=NOW(), try_login=? WHERE member_id=?'
+            );
+            $update_login->execute(array(
+                $try_login,
+                $member['member_id'],
+            ));
+        } else {
+            $error = 'fail';
+            //ロック状態にする
+            $update_login = $db->prepare(
+                'UPDATE members SET latest_login=NOW(), try_login="locked" WHERE member_id=?'
+            );
+            $update_login->execute(array($member['member_id']));
+        }
     } else {
-        $error = 'wrong';
+        //ロック状態の時のエラーメッセージ
+        $error = 'fail';
     }
 }
 require(dirname(__FILE__) . '/common/html_header.php');
@@ -37,6 +72,9 @@ require(dirname(__FILE__) . '/common/html_header.php');
                         <!-- エラーメッセージの表示 -->
                         <?php if ($error === 'wrong'): ?>
                         <p class="error"> ※e-mailアドレスとパスワードを正しく入力してください</p> 
+                        <?php endif; ?>
+                        <?php if ($error === 'fail'): ?>
+                        <p class="error"> ※時間を置いてから再度ログインしてください</p> 
                         <?php endif; ?>
                     </div>
                     <div>
